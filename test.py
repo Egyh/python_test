2114b280c07c283d5e897280f5c42d6523614164
Egyh <yuhamaguchi18@gmail.com>
2023-12-07 23:23:03 +0900

    added basic syntax

--- test.py
+++ test.py
@@ -1,3 +1,60 @@
 a = 1
 b = 5
 print (a + b)
+
+print("hello world")
+#comment
+
+x = 'abcde'
+print(x)
+
+x = ['a','b','C','d','e']
+result = x[2]
+print(result)
+
+x = ['a','b','C','d','e']
+x[1] = 'B'
+print(x)
+
+x = ['a', 'b']
+y = ['A', 'B']
+
+x.extend(y)#xにyを結合
+print(x)
+
+x = ['a','b','C','d','e']
+x_len = len(x)#リストの長さを表記
+print(x_len)
+
+x = { 'リンゴ': 150, 'バナナ': 300, 'オレンジ': 100}
+result = x['バナナ']#バナナの値段を取得
+print(result)
+
+x == 9
+if x == 10:
+    print('This is 10')
+elif x == 9:
+    print('This is not 10')
+else:
+    print('This is neither 10 or 9')
+    
+for x in range(10):#1~9までを表記
+    print(x)
+   
+x_list = [5,10,20,-5]
+for x in x_list:
+    x_twice = x * 2 #リストの要素を2倍
+    print(x_twice) 
+
+x = { 'リンゴ': 150, 'バナナ': 300, 'オレンジ': 100}
+
+for name, price in x.items():
+    print(name,price)
+    
+def average_calc(x, y):
+    print('２つの数の平均を計算する処理です')
+    avg = ( x + y) / 2
+    return avg
+
+result = average_calc(7,19)
+print(result)
