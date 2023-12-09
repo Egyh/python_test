323e4b21b07f4fdc67b1a75824336c121093c811
Egyh <yuhamaguchi18@gmail.com>
2023-12-09 18:43:48 +0900

    ファイルの操作

---
+++ code/code.py
@@ -0,0 +1,9 @@
+with open ('code.txt') as f:
+    s = f.read()
+    print(s)
+    
+x_list = ['apple', 'orange', 'banana']
+s = '\n'.join(x_list)#各要素に追加する
+
+with open('test2.txt', 'w') as f:
+    f.write(s)
