d505c2862e53f93b9cf9cb2f5fab30ef05fa4ab1
Egyh <yuhamaguchi18@gmail.com>
2023-12-09 18:44:34 +0900

    コメントアウト

--- code/code.py
+++ code/code.py
@@ -2,8 +2,8 @@ with open ('code.txt') as f:
     s = f.read()
     print(s)
     
-x_list = ['apple', 'orange', 'banana']
-s = '\n'.join(x_list)#各要素に追加する
+# x_list = ['apple', 'orange', 'banana']
+# s = '\n'.join(x_list)#各要素に追加する
 
-with open('test2.txt', 'w') as f:
-    f.write(s)
+# with open('test2.txt', 'w') as f:
+#     f.write(s)
